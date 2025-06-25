244589821199d5e2ca4f30dd5080bf4447fc710e
sayursop <albertusradya@gmail.com>
2025-06-25 16:48:40 +0700

    update_splash

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,84 @@
+import 'package:bwa_coyz/theme.dart';
+import 'package:bwa_coyz/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: whiteColor,
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: [
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Image.asset('assets/screen.png')
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 50,
+                left: 30,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 70,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/logo_new2.png'),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Text(
+                    'Temukan Kos yang \nNyaman untuk Tinggal dan Bahagia',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Stop membuang banyak waktu\npada tempat yang tidak habitable',
+                    style: greyTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  Container(
+                    width: 210,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                          builder: (context) => HomePage(),
+                        ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: blueColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(17),
+                        ),
+                      ),
+                      child: Text(
+                        'Explore Now',
+                        style: whiteTextStyle.copyWith(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
