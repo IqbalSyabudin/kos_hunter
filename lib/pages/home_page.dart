05e4bd787e7616875f0830f2b9a7e73347a971bb
alfriska <alfriska17@email.com>
2025-06-25 16:25:04 +0700

    nambah_home_page.dart

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,253 @@
+import 'package:bwa_coyz/models/space.dart';
+import 'package:bwa_coyz/models/tips.dart';
+import 'package:bwa_coyz/theme.dart';
+import 'package:bwa_coyz/widgets/bottom_navbar_item.dart';
+import 'package:bwa_coyz/widgets/tips_card.dart';
+import 'package:flutter/material.dart';
+import 'package:bwa_coyz/widgets/city_card.dart';
+import 'package:bwa_coyz/widgets/space_card.dart';
+import 'package:bwa_coyz/models/city.dart';
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: whiteColor,
+      body: SafeArea(
+        bottom: false,
+        child: ListView(
+          children: [
+            SizedBox(
+              height: edge,
+            ),
+            // NOTE: TITLE/HEADER
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                  'Explore Now',
+                      style: blackTextStyle.copyWith(
+                        fontSize: 24,
+                      ),
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Padding(
+                padding: EdgeInsets.only(left: edge),
+              child: Text(
+                'Cari kosan yang Kalcer?',
+                style: greyTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            // NOTE:  POPULAR CITIES
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                'Popular Cities',
+                style: regularTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(
+                    width: 24,
+                  ),
+                CityCard(
+                  City(
+                    id: 1,
+                    name: 'Sukoharjo',
+                    imageUrl: 'assets/image_10.png',
+                  ),
+                ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  CityCard(
+                    City(
+                      id: 2,
+                      name: 'Surakarta',
+                      imageUrl: 'assets/image_11.png',
+                      isPopular: true,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  CityCard(
+                    City(
+                      id: 1,
+                      name: 'Karanganyar',
+                      imageUrl: 'assets/pic_14.png',
+                    ),
+                  ),
+                  SizedBox(
+                    width: 24,
+                  ),
+              ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            // NOTE: RECOMENDED SPACE
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                'Recommended Space',
+                style: greyTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: edge,
+              ),
+              child: Column(
+                children: [
+                  SpaceCard(
+                    Space(
+                      id: 1,
+                      name: 'Pondok Berseri',
+                      imageUrl: 'assets/image_14.png',
+                      price: 52,
+                      city: 'Sliwedari',
+                      country: 'Surakarta',
+                      rating: 4,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  SpaceCard(
+                    Space(
+                      id: 2,
+                      name: 'Bersama Bahagia',
+                      imageUrl: 'assets/pic_13.png',
+                      price: 11,
+                      city: 'Colomadu',
+                      country: 'Karanganyar',
+                      rating: 3,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  SpaceCard(
+                    Space(
+                      id: 3,
+                      name: 'Infinty',
+                      imageUrl: 'assets/pic_12.png',
+                      price: 20,
+                      city: 'Gentan',
+                      country: 'Sukoharjo',
+                      rating: 5,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            // NOTE: TIPS & GUTDANCE
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                'Tips & Guidance',
+                style: greyTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: edge,
+              ),
+              child: Column(
+                children: [
+                  TipsCard(
+                    Tips(
+                      id: 1,
+                      title: 'Panduan Kota',
+                      imageUrl: 'assets/icon_2.png',
+                      updateAt: '20 Apr',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TipsCard(
+                    Tips(
+                      id: 2,
+                      title: 'Surakarta Fairship',
+                      imageUrl: 'assets/icon_3.png',
+                      updateAt: '11 Dec',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 50 + edge,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Container(
+        height: 65,
+        width: MediaQuery.of(context).size.width - (2 * edge),
+        margin: EdgeInsets.symmetric(
+          horizontal: edge,
+        ),
+        decoration: BoxDecoration(
+          color: Color(0xffF6F7F8),
+          borderRadius: BorderRadius.circular(23),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            BottomNavbarItem(
+              imageUrl: 'assets/Icon_home_solid.png',
+              isActive: true,
+            ),
+            BottomNavbarItem(
+              imageUrl: 'assets/Icon_mail_solid.png',
+              isActive: false,
+            ),
+            BottomNavbarItem(
+              imageUrl: 'assets/Icon_card_solid.png',
+              isActive: false,
+            ),
+            BottomNavbarItem(
+              imageUrl: 'assets/Icon_love_solid.png',
+              isActive: false,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
