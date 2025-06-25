721fc564c43a95325f8ef113213b0c73fdd34dd5
shinbysaa <shintasaa267@gmail.com>
2025-06-25 18:15:45 +0700

    tips

---
+++ lib/models/tips.dart
@@ -0,0 +1,13 @@
+class Tips {
+  final int id;
+  final String title;
+  final String imageUrl;
+  final String updateAt;
+
+  Tips({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+    required this.updateAt,
+  });
+}
