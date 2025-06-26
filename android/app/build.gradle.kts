04b1d2908aad5a4c4a19b93e949bdaaa0a55b872
IqbalSyabudin <iqbalsyabudin@gmail.com>
2025-06-26 15:31:29 +0700

    app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 android {
     namespace = "com.flussy.koshunter"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "25.2.9519653" // Ganti sesuai versi NDK yang kamu install
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -38,20 +38,23 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String?)
+            storePassword = keystoreProperties["storePassword"] as String?
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
         }
     }
 
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            // Optional: You can also enable shrinking or obfuscation
             isMinifyEnabled = false
             isShrinkResources = false
         }
+    }
+
+    packagingOptions {
+        doNotStrip("**/*.so") // Tambahan untuk cegah error strip debug symbols
     }
 }
 
